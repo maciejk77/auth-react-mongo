ebf5765f13dd8e5f1d1cd0c7f6fe997487602f09
Maciek Kurek <maciek.kurek@concentra.co.uk>
2017-09-13 11:21:07 +0100

    completed until Passport stuff

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // define our model
 const userSchema = new Schema({
@@ -7,6 +8,29 @@ const userSchema = new Schema({
   password: String
 });
 
+
+// On Save Hook, encrypt password
+// before saving a model this function
+userSchema.pre('save', function(next) {
+	// get access to user model - instance of User model (user.email, user.password)
+	const user = this;
+
+	// generate a salt then run callback
+	bcrypt.genSalt(10, function(err, salt) {
+		if(err) { return next(err); }
+
+		// hash (encrypt) our password using the salt
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+
+			if(err) { return next(err); }	
+
+			// overwrite plain text password with encrypted password
+			user.password = hash;
+			next();
+		});
+
+	});
+});
 
 // create the model class
 const ModelClass = mongoose.model('user', userSchema);
